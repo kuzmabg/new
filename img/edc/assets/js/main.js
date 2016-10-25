50aa387fe016d11e56097fac45a9d74fe6a0f05d
kuzmabg <kuzma78@yahoo.com>
2016-10-25 11:36:57 +0200

    edc

---
+++ img/edc/assets/js/main.js
@@ -0,0 +1,39 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var mm = month[today.getMonth()];
+
+var yyyy = today.getFullYear();
+if(dd<10){
+    dd='0'+dd
+} 
+if(mm<10){
+    mm='0'+mm
+} 
+var today = dd+' '+mm+' '+yyyy;
+document.getElementById('time').innerHTML=today;
+
+var today = new Date();
+
